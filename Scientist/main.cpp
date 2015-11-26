cf8ec0b1f2e8f66a8d2c5c106e08e3d78f3c4321
Christian A. Jacobsen <c.a.jacobsen95@gmail.com>
2015-11-26 19:36:43 +0000

    Experimentation with abstract vector types, not working at the moment

--- Scientist/main.cpp
+++ Scientist/main.cpp
@@ -1,14 +1,18 @@
 #include "scientist.h"
-#include <iostream>
-
-using namespace std;
 
 int main()
 {
-    scientist s1;
-    s1.getInfo();
-
-    cout << "I'm in" << endl;
+    vector<scientist> s;
+    for(unsigned int i = 0; i < 1; i++){
+        scientist temp1;
+        temp1.readInfo();
+        s.push_back(temp1);
+    }
+    for(unsigned int i = 0; i < 1; i++){
+        scientist temp2;
+        temp(temp2, s);
+        temp2.editInfo();
+    }
 
     return 0;
 }
