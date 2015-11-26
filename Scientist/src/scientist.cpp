aed482196eaeeb4a38cc993b1de05dfacc80670d
Christian A. Jacobsen <c.a.jacobsen95@gmail.com>
2015-11-26 19:37:13 +0000

    Created interface for all functions in scientist.h - do note that not all functions work appropriately

--- Scientist/src/scientist.cpp
+++ Scientist/src/scientist.cpp
@@ -4,7 +4,11 @@ scientist::scientist(){
 
 }
 
-void scientist::getInfo(){
+unsigned int scientist::size(){
+    return this->size();
+}
+
+void scientist::readInfo(){
     char gender;
     bool valid = false;
     cout << "Enter name: ";
@@ -28,4 +32,40 @@ void scientist::getInfo(){
     cin >> this->dob;
     cout << "Enter date of death(ddmmyyyy): ";
     cin >> this->dod;
+}
+
+void scientist::editInfo(){
+    char choice;
+    bool go = false;
+    do{
+        cout << "Currently stored scientists:" << endl;
+        cout << this;
+        cout << "Print again(y/n): ";
+        cin >> choice;
+        if(choice == 'y')
+            go = true;
+        else if(choice == 'n')
+            go = false;
+        else
+            cout << "Invalid input!" << endl;
+    }while(go);
+}
+
+ostream& operator<<(ostream& stream, const scientist &s){
+    stream << s.name << endl;
+    if(s.gender)
+        stream << "Female" << endl;
+    else
+        stream << "Male" << endl;
+    stream << s.dob << endl;
+    stream << s.dod << endl;
+    stream << endl;
+    return stream;
+}
+
+void temp(scientist &s1, vector<scientist> &s2){
+    s1.name = s2[0].name;
+    s1.gender = s2[0].gender;
+    s1.dob = s2[0].dob;
+    s1.dod = s2[0].dod;
 }
