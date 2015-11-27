099dae7317f5c4d987da47c9c84e25c4c29e4f9c
Þórmundur Smári Hilmarsson <Thormundur15@gmail.com>
2015-11-27 10:05:17 +0000

    Fix

--- Scientist/main.cpp
+++ Scientist/main.cpp
@@ -8,11 +8,6 @@ int main()
         temp1.readInfo();
         s.push_back(temp1);
     }
-    for(unsigned int i = 0; i < 1; i++){
-        scientist temp2;
-        temp(temp2, s);
-        temp2.editInfo();
-    }
 
     return 0;
 }
