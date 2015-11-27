0bd4e13f8273346c46a39e15d4f54a44726407e1
Hekla Rún Ámundadóttir <heklaruna@gmail.com>
2015-11-27 11:46:01 +0000

    Created core program with link to class functions.

--- Scientist/main.cpp
+++ Scientist/main.cpp
@@ -1,13 +1,49 @@
 #include "scientist.h"
 
+void core(vector<scientist> &s);
+
 int main()
 {
     vector<scientist> s;
-    for(unsigned int i = 0; i < 1; i++){
-        scientist temp1;
-        temp1.readInfo();
-        s.push_back(temp1);
-    }
+
+    core(s);
 
     return 0;
+}
+
+void core(vector<scientist> &s)
+{
+    bool go = false;
+    bool check = false;
+    do{
+        go = false;
+        char choice;
+        cout << "Enter a number for your choice. Any other inputs quit the program." << endl;
+        cout << endl;
+        cout << "1. Add scientist\t2. Edit scientist" << endl;
+        cout << "Your choice: ";
+        cin >> choice;
+        if(choice == '1'){
+            scientist temp;
+            temp.readInfo();
+            s.push_back(temp);
+            go = true;
+        }
+        else if(choice == '2'){
+            check = false;
+            string in;
+            int choice = 99;
+            cin >> in;
+            for(unsigned int i = 0; i < s.size(); i++){
+                if(in == retName(s[i]))
+                    choice = i;
+                    check = true;
+            }
+            if(check)
+                s[choice].editInfo();
+            else if(!check)
+                cout << "No such scientist!";
+            go = true;
+        }
+    }while(go);
 }
