fa0a4e5a1737dde7a6a8ab43c00086af416a3e9a
Þórmundur Smári Hilmarsson <Thormundur15@gmail.com>
2015-12-11 10:20:17 +0000

    LOGO!!!!

--- GottBull/mainwindow.cpp
+++ GottBull/mainwindow.cpp
@@ -3,12 +3,13 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow){
     ui->setupUi(this);
+
+    QPixmap logo("C:/Users/ÞórmundurSmári/VN1Group10/GottBull/logo.png");
+    ui->labelPic->setPixmap(logo);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
 }
